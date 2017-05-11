1339dde53dd1021e8f235ce96ec2578933526125
dfasani <david.fasani@gmail.com>
2017-05-11 16:25:42 +0200

    premiere version

---
+++ compute_routing.py
@@ -0,0 +1,198 @@
+import math
+
+from ortools.constraint_solver import pywrapcp, routing_enums_pb2
+from datatools import list_companies, insert_result, get_distance_matrix
+import logging
+import googlemaps
+import urllib.request, json
+
+correspondance_case_identreprise = []
+
+
+class CreateDistanceCallback(object):
+    """Create callback to calculate distances between points."""
+
+    def __init__(self, locations):
+        """Initialize distance array."""
+
+        self.matrix = get_distance_matrix()
+
+    def distance_osrm(self, locations):
+        weburl = "http://router.project-osrm.org/table/v1/driving/"
+        for l in locations:
+            weburl = weburl + str(l[0]) + "," + str(l[1]) + ";"
+
+        # je retire le dernier ; ajoute a la fin
+        weburl = weburl[:-1]
+        with urllib.request.urlopen(weburl) as url:
+            data = json.loads(url.read().decode())
+            self.matrix = data["durations"]
+
+    def distance_google(self, locations):
+
+        # origins = list(locations)
+        # destinations = list(locations)
+
+        client = googlemaps.Client("AIzaSyD0hkj4gC-MwkLwLx0dgvjt4XKuI-tk_bg")
+
+        # matrix = client.distance_matrix(origins, destinations)
+
+        num_locations = len(locations)
+        self.matrix = {}
+
+        for from_node in range(num_locations):
+            self.matrix[from_node] = {}
+            for to_node in range(num_locations):
+                if from_node == to_node:
+                    self.matrix[from_node][to_node] = 0
+                else:
+                    p1 = locations[from_node]
+                    p2 = locations[to_node]
+
+                    print(from_node, to_node)
+
+                    self.matrix[from_node][to_node] = client.distance_matrix([p1], [p2])
+
+    def Distance(self, from_node, to_node):
+
+        dep=correspondance_case_identreprise[from_node]
+        arr=correspondance_case_identreprise[to_node]
+
+        return self.matrix[dep][arr]
+
+
+
+# Demand callback
+class CreateDemandCallback(object):
+    """Create callback to get demands at each location."""
+
+    def __init__(self, demands):
+        self.matrix = demands
+
+    def Demand(self, from_node, to_node):
+        return self.matrix[from_node]
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    # Create the data.
+    locations, demands = create_data_array()
+
+    num_locations = len(locations)
+    depot = 0  # The depot is the start and end point of each route.
+    num_vehicles = math.ceil((num_locations - 1) / 5)
+
+    # Create routing model.
+    if num_locations > 0:
+
+        logging.info("Calcul des meilleurs trajets avec %s sites et %s visiteurs", num_locations, num_vehicles)
+
+        routing = pywrapcp.RoutingModel(num_locations, num_vehicles, depot)
+        search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
+
+        # Setting first solution heuristic: the
+        # method for finding a first solution to the problem.
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+        # The 'PATH_CHEAPEST_ARC' method does the following:
+        # Starting from a route "start" node, connect it to the node which produces the
+        # cheapest route segment, then extend the route by iterating on the last
+        # node added to the route.
+
+        # Put a callback to the distance function here. The callback takes two
+        # arguments (the from and to node indices) and returns the distance between
+        # these nodes.
+
+        dist_between_locations = CreateDistanceCallback(locations)
+        dist_callback = dist_between_locations.Distance
+        routing.SetArcCostEvaluatorOfAllVehicles(dist_callback)
+
+        # Put a callback to the demands.
+        demands_at_locations = CreateDemandCallback(demands)
+        demands_callback = demands_at_locations.Demand
+
+        # Add a dimension for demand.
+        slack_max = 0
+        vehicle_capacity = 5
+        fix_start_cumul_to_zero = True
+        demand = "Demand"
+        routing.AddDimension(demands_callback, slack_max, vehicle_capacity,
+                             fix_start_cumul_to_zero, demand)
+
+        # Solve, displays a solution if any.
+        assignment = routing.SolveWithParameters(search_parameters)
+        if assignment:
+            # Display solution.
+            # Solution cost.
+            print("Total distance of all routes: " + str(assignment.ObjectiveValue()) + "\n")
+
+            result_by_vehicule = []
+
+            for vehicle_nbr in range(num_vehicles):
+                index = routing.Start(vehicle_nbr)
+                index_next = assignment.Value(routing.NextVar(index))
+                route = ''
+                route_dist = 0
+                route_demand = 0
+
+                while not routing.IsEnd(index_next):
+                    node_index = routing.IndexToNode(index)
+                    node_index_next = routing.IndexToNode(index_next)
+                    route += str(node_index) + " -> "
+                    # Add the distance to the next node.
+                    route_dist += dist_callback(node_index, node_index_next)
+                    # Add demand.
+                    route_demand += demands[node_index_next]
+                    index = index_next
+                    index_next = assignment.Value(routing.NextVar(index))
+
+                node_index = routing.IndexToNode(index)
+                node_index_next = routing.IndexToNode(index_next)
+                route += str(node_index) + " -> " + str(node_index_next)
+                route_dist += dist_callback(node_index, node_index_next)
+                print("Route for vehicle " + str(vehicle_nbr) + ":\n\n" + route + "\n")
+                print("Distance of route " + str(vehicle_nbr) + ": " + str(route_dist))
+                print("Demand met by vehicle " + str(vehicle_nbr) + ": " + str(route_demand) + "\n")
+
+                result_by_vehicule.append((vehicle_nbr, route, route_dist, route_demand))
+
+            insert_result(assignment.ObjectiveValue(), num_locations, num_vehicles, result_by_vehicule,
+                          correspondance_case_identreprise)
+
+        else:
+            print('No solution found.')
+    else:
+        print('Specify an instance greater than 0.')
+
+
+def create_data_array():
+
+    locations = []
+
+    logging.info("recuperation des entreprises")
+
+
+    for row in list_companies():
+
+        # a la case i on trouve l'entreprise n° x
+        correspondance_case_identreprise.append(row[0])
+
+        # locations.append((row[4], row[5]))
+
+        nom = row[1]
+        cp = row[2]
+        ville = row[3]
+
+        adresse = nom + ", " + cp + ", " + ville + ", France "
+        locations.append(adresse)
+
+    demands = [1] * len(locations)
+    demands[0] = 0
+
+    return locations, demands
+
+
+if __name__ == '__main__':
+    main()
---
+++ datatools.py
@@ -0,0 +1,278 @@
+#!/usr/bin/python
+import geocoder
+import sqlite3
+import logging
+import googlemaps
+import pprint
+
+
+def compute_distances_between_companies():
+    correspondance_case_identreprise = []
+
+    locations = []
+
+    for row in list_companies():
+        # if (len(locations) > 5):
+        #    break;
+
+        # a la case i on trouve l'entreprise n° x
+        correspondance_case_identreprise.append(row[0])
+
+        # locations.append((row[4], row[5]))
+
+        nom = row[1]
+        cp = row[2]
+        ville = row[3]
+
+        adresse = nom + ", " + cp + ", " + ville + ", France "
+        locations.append(adresse)
+
+    matrix = {}
+
+    client = googlemaps.Client("AIzaSyD0hkj4gC-MwkLwLx0dgvjt4XKuI-tk_bg")
+
+    num_locations = len(locations)
+
+    # Open database connection
+    conn = sqlite3.connect('missionexpertise.db')
+
+    # prepare a cursor object using cursor() method
+    cursor = conn.cursor()
+
+    for from_node in range(num_locations):
+        matrix[from_node] = {}
+        for to_node in range(num_locations):
+            if from_node == to_node:
+                matrix[from_node][to_node] = 0
+            else:
+                p1 = locations[from_node]
+                p2 = locations[to_node]
+
+                distance_matrix = client.distance_matrix([p1], [p2], mode="driving")
+
+                print(from_node, to_node, distance_matrix)
+
+                # print("a",distance_matrix["rows"])
+                # print("b",distance_matrix["rows"][0])
+                # print("c",distance_matrix["rows"][0]["elements"])
+                # print("d",distance_matrix["rows"][0]["elements"][0]["distance"])
+                # print("e",distance_matrix["rows"][0]["elements"][0]["distance"]["value"])
+
+                distance = int(distance_matrix["rows"][0]["elements"][0]["distance"]["value"])
+                matrix[from_node][to_node] = distance
+
+                cursor.execute("INSERT INTO distance_entreprise VALUES ('%s','%s','%s')" % (
+                correspondance_case_identreprise[from_node], correspondance_case_identreprise[to_node], distance))
+
+    conn.commit()
+    conn.close()
+
+
+def update_companies_latlng():
+    logging.info("maj des coordonnées GPS des entreprises...")
+
+    # Open database connection
+    conn = sqlite3.connect('missionexpertise.db')
+
+    # prepare a cursor object using cursor() method
+    cursor = conn.cursor()
+
+    for row in cursor.execute('SELECT * FROM entreprise WHERE lat IS NULL'):
+
+        nom = row[1]
+        cp = row[2]
+        ville = row[3]
+
+        adresse = nom + ", " + cp + ", " + ville + ", France "
+
+        coordonnees = geocoder.google(adresse).latlng
+
+        print(row, coordonnees)
+
+        # le geocoding ne fonctionne pas parfois, on laisse qques chances
+        if len(coordonnees) == 0:
+            for _ in range(2):
+                if len(coordonnees) == 0:
+                    # sleep(randint(1,3))
+                    coordonnees = geocoder.google(adresse).latlng
+
+        if len(coordonnees) == 0:
+            raise ValueError("adresse introuvable", adresse)
+
+        sqlUpdate = 'UPDATE entreprise  SET lat={} , lng={} WHERE id={}'.format(coordonnees[0], coordonnees[1], row[0])
+        conn.cursor().execute(sqlUpdate)
+
+        conn.commit()
+
+    # disconnect from server
+    conn.close()
+
+    logging.info("maj des coordonnées GPS des entreprises   [OK]")
+
+
+def get_distance_matrix():
+    matrix = {}
+
+    # Open database connection
+    conn = sqlite3.connect('missionexpertise.db')
+
+    # prepare a cursor object using cursor() method
+    cursor = conn.cursor()
+
+    for row in cursor.execute('SELECT * FROM distance_entreprise order by id_entreprise_depart, id_entreprise_arrivee'):
+        id_dep = row[0]
+        id_arr = row[1]
+        dist = row[2]
+
+        if not id_dep in matrix:
+            matrix[id_dep] = {}
+            matrix[id_dep][id_dep] = 0
+
+        matrix[id_dep][id_arr] = int(dist)
+
+    # disconnect from server
+    conn.close()
+
+    #pp = pprint.PrettyPrinter(indent=4)
+    #pp.pprint(matrix)
+
+    return matrix
+
+
+def list_companies():
+    results = []
+
+    # Open database connection
+    conn = sqlite3.connect('missionexpertise.db')
+
+    # prepare a cursor object using cursor() method
+    cursor = conn.cursor()
+
+    for row in cursor.execute('SELECT * FROM entreprise ORDER BY nom'):
+        results.append(row)
+
+    # disconnect from server
+    conn.close()
+
+    return results
+
+
+def list_demand_by_locations():
+    results = []
+
+    # Open database connection
+    conn = sqlite3.connect('missionexpertise.db')
+
+    # prepare a cursor object using cursor() method
+    cursor = conn.cursor()
+
+    sql = '''
+        select entreprise.id, entreprise.nom, entreprise.cp, entreprise.ville, count(*) as nb_eleves
+        from eleve, entreprise
+        where eleve.id_entreprise = entreprise.id
+        group by entreprise.nom, entreprise.cp, entreprise.ville
+        order by entreprise.nom, entreprise.cp, entreprise.ville
+    '''
+
+    for row in cursor.execute(sql):
+        results.append(row)
+
+    # disconnect from server
+    conn.close()
+
+    return results
+
+
+def list_students():
+    results = []
+
+    # Open database connection
+    conn = sqlite3.connect('missionexpertise.db')
+
+    # prepare a cursor object using cursor() method
+    cursor = conn.cursor()
+
+    for row in cursor.execute('SELECT * FROM eleve ORDER BY nom'):
+        results.append(row)
+
+    # disconnect from server
+    conn.close()
+
+    return results
+
+
+def insert_result(distanceTotale, num_locations, num_vehicles, result_by_vehicule, correspondance_case_identreprise):
+    # Open database connection
+    conn = sqlite3.connect('missionexpertise.db')
+
+    # prepare a cursor object using cursor() method
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO calcul(distance_totale,nb_sites,nb_visiteurs) VALUES ('%s','%s','%s')" % (
+        distanceTotale, num_locations, num_vehicles))
+
+    idcalcul = cursor.lastrowid
+
+    for result in result_by_vehicule:
+        vehicle_nbr, route, route_dist, route_demand = result
+        cursor.execute(
+            "INSERT INTO distance(id_calcul,num_vehicule,distance,nb_etapes) VALUES ('%s','%s','%s','%s')" % (
+                idcalcul, vehicle_nbr, route_dist, route_demand))
+
+        etapes = route.split(" -> ")
+
+        for i, etape in enumerate(etapes[1:-1]):
+            etape = int(etape)
+            cursor.execute(
+                "INSERT INTO etape(id_calcul,num_vehicule,id_entreprise,ordre) VALUES ('%s','%s','%s','%d')" % (
+                    idcalcul, vehicle_nbr, correspondance_case_identreprise[etape], i))
+
+    # enregistre les changements
+    conn.commit()
+
+    # disconnect from server
+    conn.close()
+
+
+if __name__ == '__main__':
+
+    # compute_distances_between_companies()
+    get_distance_matrix()
+
+    print()
+    print("-- entreprises --")
+    print()
+
+    for row in list_companies():
+        nom = row[1]
+        cp = row[2]
+        ville = row[3]
+
+        # Now print fetched result
+        print("nom=%s,cp=%s,ville=%s" % (nom, cp, ville))
+
+    print()
+    print("-- etudiants --")
+    print()
+
+    for row in list_students():
+        nom = row[1]
+        prenom = row[2]
+
+        # Now print fetched result
+        print("nom=%s,prenom=%s" % (nom, prenom))
+
+    print()
+    print("-- entreprises --")
+    print()
+
+    for row in list_demand_by_locations():
+        nom = row[1]
+        cp = row[2]
+        ville = row[3]
+        demande = row[4]
+
+        # Now print fetched result
+        print("nom=%s,cp=%s,ville=%s,demande=%s" % (nom, cp, ville, demande))
+
+    update_companies_latlng()
